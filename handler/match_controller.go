2e5ab2fbc078e747b89491596ec7afc884b0a506
zhanghao <zhanghao@microbeam.cn>
2023-06-19 10:24:48 +0800

    first commit

--- handler/match_controller.go
+++ handler/match_controller.go
@@ -16,33 +16,3 @@ func (e *Match_controller) Call(ctx context.Context, req *match_controller.Reque
 	rsp.Msg = "Hello " + req.Name
 	return nil
 }
-
-// Stream is a server side stream handler called via client.Stream or the generated client code
-func (e *Match_controller) Stream(ctx context.Context, req *match_controller.StreamingRequest, stream match_controller.Match_controller_StreamStream) error {
-	log.Infof("Received Match_controller.Stream request with count: %d", req.Count)
-
-	for i := 0; i < int(req.Count); i++ {
-		log.Infof("Responding: %d", i)
-		if err := stream.Send(&match_controller.StreamingResponse{
-			Count: int64(i),
-		}); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// PingPong is a bidirectional stream handler called via client.Stream or the generated client code
-func (e *Match_controller) PingPong(ctx context.Context, stream match_controller.Match_controller_PingPongStream) error {
-	for {
-		req, err := stream.Recv()
-		if err != nil {
-			return err
-		}
-		log.Infof("Got ping %v", req.Stroke)
-		if err := stream.Send(&match_controller.Pong{Stroke: req.Stroke}); err != nil {
-			return err
-		}
-	}
-}
