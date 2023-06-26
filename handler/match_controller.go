e5b3fcc0c899b7ee5df9d08740f7057812445136
zhanghao <zhanghao@microbeam.cn>
2023-06-26 17:49:46 +0800

    log

--- handler/match_controller.go
+++ handler/match_controller.go
@@ -2,15 +2,19 @@ package handler
 
 import (
 	"context"
+	"time"
 
 	"match_controller/controller"
 	match_controller "match_controller/proto"
+
+	"github.com/micro/micro/v3/service/logger"
 )
 
 type Match_controller struct{}
 
 // Call is a single request handler called via client.Call or the generated client code
 func (e *Match_controller) AddTask(ctx context.Context, req *match_controller.AddTaskReq, rsp *match_controller.AddTaskRsp) error {
+	logger.Infof("AddTask %v", time.Now().UnixNano()/1e6)
 	controller.DefaultManager.AddTask(req.GameId)
 	return nil
 }
